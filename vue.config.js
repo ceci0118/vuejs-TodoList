6d36cd03f465bfdb0fab4271bf568df651d632f9
ceci0118 <43018306+ceci0118@users.noreply.github.com>
2020-12-12 18:28:13 -0500

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+// Vue.config.js 配置选项
+
+module.exports = {
+
+        // 选项
+    
+        //  基本路径
+    
+        publicPath: "./",
+    
+        //  构建时的输出目录
+    
+        outputDir: "dist",
+    
+        //  放置静态资源的目录
+    
+        assetsDir: "static",
+    
+        //  html 的输出路径
+    
+        indexPath: "index.html",
+    
+        //文件名哈希
+    
+        filenameHashing: true,
+    
+
+    
+    }
